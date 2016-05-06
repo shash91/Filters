90f09703bc360fe61969174160ba21c8b878ebd6
shash91 <shashika@davton.com>
2016-05-06 14:35:28 +0530

    Using autnorization filters
    Applied a Custom Authorization Filter

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+           [CustomAuth(true)]
+        public string Index()
+        {
+            return "This is the Index action on the Home controller";
+        }
+    }
+}
